7a6a56eaa352751c0a7ffa0e22c62d3b1757d6c0
NHG <nhg@posteo.de>
2019-11-16 19:32:46 +0100

    use tty1 instead of tty0 for output

--- scripts/c14hcopy-mic.sh
+++ scripts/c14hcopy-mic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 CPYDIR="${TMPDIR?}"/$(date --iso-8601)
-mkdir -p "$CPYDIR" 2>/dev/tty0
-find "$MICDIR" -mindepth 1 -ctime 0 -execdir cp '{}' "$CPYDIR" ';' 2>/dev/tty0
+mkdir -p "$CPYDIR" 2>/dev/tty1
+find "$MICDIR" -mindepth 1 -ctime 0 -execdir cp '{}' "$CPYDIR" ';' 2>/dev/tty1
