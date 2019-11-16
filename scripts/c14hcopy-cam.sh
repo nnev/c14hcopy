dd2ee8b90202312dd5df15e112a4546216402756
NHG <nhg@posteo.de>
2019-11-16 22:17:57 +0100

    output to irc

--- scripts/c14hcopy-cam.sh
+++ scripts/c14hcopy-cam.sh
@@ -2,4 +2,5 @@
 set -e
 CPYDIR="${TMPDIR?}"/$(date --iso-8601)
 mkdir -p "$CPYDIR"
-find "$CAMDIR" -mindepth 1 -ctime -1 -execdir cp -v '{}' "$CPYDIR" ';' > /irc/chaos-hd
+find "$CAMDIR" -mindepth 1 -ctime -1 -execdir cp '{}' "$CPYDIR" ';'
+echo "Finished copying cam files from today. It is now safe to remove the Camera." > /irc/chaos-hd
--- scripts/c14hcopy-mic.sh
+++ scripts/c14hcopy-mic.sh
@@ -2,4 +2,5 @@
 set -e
 CPYDIR="${TMPDIR?}"/$(date --iso-8601)
 mkdir -p "$CPYDIR"
-find "$MICDIR" -mindepth 1 -ctime -1 -execdir cp -v '{}' "$CPYDIR" ';' > /irc/chaos-hd
+find "$MICDIR" -mindepth 1 -ctime -1 -execdir cp -v '{}' "$CPYDIR" ';'
+echo "Finished copying mic files from today. It is now safe to remove the Mic." > /irc/chaos-hd
