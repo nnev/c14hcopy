cac74c2198f683106a5bc250d9f10e9d7df1f261
NHG <nhg@posteo.de>
2019-11-16 21:57:49 +0100

    fix service, add info output

--- scripts/c14hcopy-mic.sh
+++ scripts/c14hcopy-mic.sh
@@ -2,4 +2,4 @@
 set -e
 CPYDIR="${TMPDIR?}"/$(date --iso-8601)
 mkdir -p "$CPYDIR" 2>/dev/tty1
-find "$MICDIR" -mindepth 1 -ctime 0 -execdir cp '{}' "$CPYDIR" ';' 2>/dev/tty1
+find "$MICDIR" -mindepth 1 -ctime -1 -execdir cp '{}' "$CPYDIR" ';' -execdir echo "copying '{}'" ';' 2>/dev/tty1
