7375ce94fa12c6e44b3358071b3f88b41f93313f
NHG <nhg@posteo.de>
2019-11-16 22:04:07 +0100

    write to irc

--- scripts/c14hcopy-cam.sh
+++ scripts/c14hcopy-cam.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 CPYDIR="${TMPDIR?}"/$(date --iso-8601)
-mkdir -p "$CPYDIR" 2>/dev/tty1
-find "$CAMDIR" -mindepth 1 -ctime -1 -execdir cp '{}' "$CPYDIR" ';' -execdir echo "copying '{}'" ';' 2>/dev/tty1
+mkdir -p "$CPYDIR"
+find "$CAMDIR" -mindepth 1 -ctime -1 -execdir cp '{}' "$CPYDIR" ';' -execdir echo "copying '{}'" '>' /irc/chaos-hd ';'
--- scripts/c14hcopy-mic.sh
+++ scripts/c14hcopy-mic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 CPYDIR="${TMPDIR?}"/$(date --iso-8601)
-mkdir -p "$CPYDIR" 2>/dev/tty1
-find "$MICDIR" -mindepth 1 -ctime -1 -execdir cp '{}' "$CPYDIR" ';' -execdir echo "copying '{}'" ';' 2>/dev/tty1
+mkdir -p "$CPYDIR"
+find "$MICDIR" -mindepth 1 -ctime -1 -execdir cp '{}' "$CPYDIR" ';' -execdir echo "copying '{}'" '>' /irc/chaos-hd ';'
