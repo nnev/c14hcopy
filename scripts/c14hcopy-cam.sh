2bc6d823f3ee242ea6a4eee8473f9aed301f8623
NHG <nhg@posteo.de>
2019-12-12 23:13:58 +0100

    unmount device

--- scripts/c14hcopy-cam.sh
+++ scripts/c14hcopy-cam.sh
@@ -3,4 +3,5 @@ set -e
 CPYDIR="${TMPDIR?}"/$(date --iso-8601)
 mkdir -p "$CPYDIR"
 find "$CAMDIR" -mindepth 1 -mtime -1 -execdir cp '{}' "$CPYDIR" ';'
+umount /mnt/cam -l
 echo "Finished copying cam files from today. It is now safe to remove the Camera." > /irc/chaos-hd
--- scripts/c14hcopy-mic.sh
+++ scripts/c14hcopy-mic.sh
@@ -3,4 +3,5 @@ set -e
 CPYDIR="${TMPDIR?}"/$(date --iso-8601)
 mkdir -p "$CPYDIR"
 find "$MICDIR" -mindepth 1 -mtime -1 -execdir cp -v '{}' "$CPYDIR" ';'
+umount /mnt/mic -l
 echo "Finished copying mic files from today. It is now safe to remove the Mic." > /irc/chaos-hd
