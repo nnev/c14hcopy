35c4e36bc6edffcaf8c78c35546b0634c7c54179
NHG <nhg@posteo.de>
2019-12-12 23:06:20 +0100

    use mtime instead of ctime

--- scripts/c14hcopy-cam.sh
+++ scripts/c14hcopy-cam.sh
@@ -2,5 +2,5 @@
 set -e
 CPYDIR="${TMPDIR?}"/$(date --iso-8601)
 mkdir -p "$CPYDIR"
-find "$CAMDIR" -mindepth 1 -ctime -1 -execdir cp '{}' "$CPYDIR" ';'
+find "$CAMDIR" -mindepth 1 -mtime -1 -execdir cp '{}' "$CPYDIR" ';'
 echo "Finished copying cam files from today. It is now safe to remove the Camera." > /irc/chaos-hd
--- scripts/c14hcopy-mic.sh
+++ scripts/c14hcopy-mic.sh
@@ -2,5 +2,5 @@
 set -e
 CPYDIR="${TMPDIR?}"/$(date --iso-8601)
 mkdir -p "$CPYDIR"
-find "$MICDIR" -mindepth 1 -ctime -1 -execdir cp -v '{}' "$CPYDIR" ';'
+find "$MICDIR" -mindepth 1 -mtime -1 -execdir cp -v '{}' "$CPYDIR" ';'
 echo "Finished copying mic files from today. It is now safe to remove the Mic." > /irc/chaos-hd
